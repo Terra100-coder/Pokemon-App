9a02856bf2253c772787128b402f86342cc7da02
Terra_D <djakiss.terra100@gmail.com>
2025-09-20 17:40:17 +0000

    Creation de l'app pokedex et ajout du composant

---
+++ Pokemon-App/src/app/app.ts
@@ -0,0 +1,34 @@
+import { Component, computed, signal } from '@angular/core';
+import { POKEMON_LIST } from './pokemon-list';
+import { Pokemon } from './pokemon.model';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+
+  pokemonList = signal(POKEMON_LIST);
+
+  size (pokemon: Pokemon) {
+    if (pokemon.life <= 15) {
+      return 'Petit';
+    }
+
+    if (pokemon.life >= 25) {
+      return 'Grand';
+    }
+
+    return 'Moyen';
+  }
+
+  incrementLife(pokemon: Pokemon) {
+    pokemon.life = pokemon.life + 1;
+  }
+
+  decrementLife(pokemon: Pokemon) {
+    pokemon.life = pokemon.life - 1;
+  }
+}
